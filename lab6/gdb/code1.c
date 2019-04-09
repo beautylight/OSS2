b7df1db7353078eaa7908948a46499adbd939f61
jiina28 <21800753@handong.edu>
2019-04-09 12:03:37 +0900

    12:04

--- lab6/gdb/code1.c
+++ lab6/gdb/code1.c
@@ -5,7 +5,7 @@ int main(void){
   int sum = 0;
   int i;
 
-  for(i = 1; i <= 10; i++);
+  for(i = 1; i <= 10; i++)
     sum += i;
 
   printf("sum=%d\n", sum);
--- lab6/gdb/code2.c
+++ lab6/gdb/code2.c
@@ -5,7 +5,7 @@ int main(void){
   int count = 0;
 
   int is_looping = 1; // sentinel for while()
-  while(is_looping = 1){
+  while(is_looping == 1){
     count++;
     if(count > 10000000)  is_looping = 0;
   }
